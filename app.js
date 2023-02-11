664ba73c64ac4263260446bb873cafde18e7b8ec
Rebecca Reyes <rebreyaz@gmail.com>
2023-02-11 15:32:04 -0700

    update post request to delete custom list items

--- app.js
+++ app.js
@@ -62,6 +62,15 @@ const item3 = new Item({
 // Store documents in an array
 const defaultItems = [item1, item2, item3];
 
+// Create a Mongoose SCHEMA for dynamic route
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema],
+});
+
+// Create a Mongoose Model for dynamic route
+const List = mongoose.model("List", listSchema);
+
 // Create a GET request on the homepage route
 app.get("/", function (req, res) {
   // Save user input to Items Collection, passing results to callback (stored in foundItems)
@@ -88,28 +97,104 @@ app.get("/", function (req, res) {
 // Handle the post request to the homepage route
 app.post("/", function (req, res) {
   // save the text that the user types
-  const itemName = req.body.newItem;
+  const itemName = req.body.newItem; // newItem is the name of the input in our list.ejs file
+  const listName = req.body.list; // list is the name of the button in our list.ejs file
 
   // Create a new Mongoose DOCUMENT that contains the user's input (stored in itemName)
   const item = new Item({
     name: itemName,
   });
 
-  // Save the document in the Items Collection
-  item.save();
+  // Check if the listName that triggered the post request is equal to "Today"
+  if (listName === "Today") {
+    // Save the document in the Items Collection
+    item.save();
 
-  // redirect to home route to render itemName on the app.
-  res.redirect("/");
+    // redirect to home route to render itemName on the app.
+    res.redirect("/");
+  } else {
+    // search list document in the lists collection in our database
+    List.findOne({ name: listName }, function (err, foundList) {
+      // add the item and push it in the existing array of items
+      foundList.items.push(item);
+      // Save the foundList document
+      foundList.save();
+      // redirect to listName (custom list path)
+      res.redirect("/" + listName);
+    });
+  }
 });
 
 // Handle the post request for deleted items
 app.post("/delete", function (req, res) {
-  console.log(req.body);
+  // Create a constant called checkedItemId set it equal to the checkbox from input's name list.ejs
+  const checkedItemId = req.body.checkbox;
+
+  // Create a constant called listName set it equal to the listName from input's name from list.ejs
+  const listName = req.body.listName;
+
+  // Check if we are making a delete request from the default list called Today or the custom list
+  if (listName === "Today") {
+    // Delete the item from the database that has checked off ID
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log("Successfully deleted checked item.");
+        // redirect to home route to delete item on the app.
+        res.redirect("/");
+      }
+    });
+  } else {
+    // Find the list document that has the current listName
+    // pull from items array that has an ID that corresponds to the checkedItemID
+    // use a callback function that gives you an error and a foundList
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        // if there are error throw the error
+        if (err) {
+          throw err;
+        }
+
+        // if there are no errors
+        if (!err) {
+          // then redirect to listName (custom list path)
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
 });
 
-// Create a GET request for the work page
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+// Create a GET request for a dynamic route by using express
+app.get("/:customListName", function (req, res) {
+  const customListName = req.params.customListName;
+
+  // Check if there's a list that has the same name as the one that the user is currently tying to access
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        // create a new List by creating a Mongoose Document for dynamic route when the user tries to access a customListName
+        // Make sure you create a Mongoose Schema and Model above
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+
+        // Save list document
+        list.save();
+
+        // redirect to customListName
+        res.redirect("/" + customListName);
+      } else {
+        // Show an existing List, render list.ejs, foundList.name, and foundList.items
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
 });
 
 // Create a GET request for the about page
